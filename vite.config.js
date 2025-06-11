76326679cdd3a8496b40a778bb7c9c79730b4b00
Keshav Abhishek <crystaled2003@gmail.com>
2025-06-11 19:40:38 +0530

    Icon

--- vite.config.js
+++ vite.config.js
@@ -5,32 +5,32 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
-      manifest: {
-        name: 'My Dictionary App',
-        short_name: 'Dictionary',
-        start_url: '.',
-        display: 'fullscreen',
-        background_color: '#ffffff',
-        theme_color: '#45bcf2',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
+            manifest: {
+                name: 'My Dictionary App',
+                short_name: 'Dictionary',
+                start_url: '.',
+                display: 'fullscreen',
+                background_color: '#ffffff',
+                theme_color: '#45bcf2',
+                icons: [
+                    {
+                        src: 'icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
 })
